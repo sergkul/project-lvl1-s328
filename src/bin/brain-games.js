c0ec4f1e12f657fed699fda4637b01a7afac01ac
sergkul <mail@sergk.ru>
2018-08-21 13:05:57 +0300

    Linter added

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { greetings } from '..';
+import greetings from '..';
 
 greetings();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const greetings = () => {
-  console.log(`Welcome to the Brain Games!\n`);
+const greetings = () => {
+  console.log('Welcome to the Brain Games!\n');
 
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
 };
+
+export default greetings;
