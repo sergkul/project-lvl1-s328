dd72a0725564de820003d33541892fc8cb2075cb
sergkul <mail@sergk.ru>
2018-08-22 09:57:19 +0300

    Brain Even Game release

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Test Coverage](https://api.codeclimate.com/v1/badges/4dfb8fcf0427e20cd951/test_coverage)](https://codeclimate.com/github/sergkul/project-lvl1-s328/test_coverage)
 
 [![Build Status](https://travis-ci.org/sergkul/project-lvl1-s328.svg?branch=master)](https://travis-ci.org/sergkul/project-lvl1-s328)
+
+https://asciinema.org/a/vzeaxTjJ2cNyPrDT42QbB2T6r
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import readlineSync from 'readline-sync';
 
-const greetings = () => {
-  console.log('Welcome to the Brain Games!\n');
+const makeNumber = () => Math.floor(Math.random() * 100 + 1);
 
-  const name = readlineSync.question('May I have your name? ');
+const isEven = n => n % 2 === 0;
 
-  console.log(`Hello, ${name}!`);
+const rounds = 3;
+
+const playRounds = () => {
+  for (let i = 1; i <= rounds; i += 1) {
+    const question = makeNumber();
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
+
+    console.log(`Question: ${question}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer !== rightAnswer) {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
+      return false;
+    }
+
+    console.log('Correct');
+  }
+
+  return true;
 };
 
-export default greetings;
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!\n`);
+
+  const isWin = playRounds();
+
+  if (isWin) {
+    console.log(`Congratulations, ${username}!`);
+  } else {
+    console.log(`Let's try again, ${username}!`);
+  }
+};
+
+export default game;
