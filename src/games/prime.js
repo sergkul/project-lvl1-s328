b7b299ed5b388f9e4ffffebb4288cd1bd024e6fd
sergkul <mail@sergk.ru>
2018-08-26 20:08:54 +0300

    'isPrime' function fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,6 +5,8 @@ import game from '..';
 const description = 'Is this number prime?';
 
 const isPrime = (number) => {
+  if (number < 2) return false;
+
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) return false;
   }
